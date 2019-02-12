c22629a37c85545087de466432496a4dadccc154
Ruslan <mckruslan@gmail.com>
2019-02-12 14:22:00 +0200

    Fixed mistake with Dark knights heal.

--- Homework_6/src/fighters/DarkKnight.java
+++ Homework_6/src/fighters/DarkKnight.java
@@ -14,6 +14,22 @@ public class DarkKnight extends BaseFighter implements PostFightActions, PreFigh
         setHealth(getHealth() + ((getFullHP() - getHealth()) / 2));
     }
 
+    private void countRegen(int regenValue) {
+        setHealth(getHealth() + (regenValue / 2));
+        if (getHealth() > getFullHP()) {
+            setHealth(getFullHP());
+        }
+    }
+
+    @Override
+    public int attack(BaseFighter opponentTwo) {
+        int healthBeforeAttack = opponentTwo.getHealth();
+        opponentTwo.takeDamage(this.getAttack());
+        int regenValue = healthBeforeAttack - opponentTwo.getHealth();
+        countRegen(regenValue);
+        return this.getAttack();
+    }
+
     @Override
     public void actionWithFight(BaseFighter fighter) {
         setFullHP(this);
