def205cf78b9714e6f4d9a0189d1de9d677908f0
Ruslan <mckruslan@gmail.com>
2019-02-14 19:10:21 +0200

    Fixed imbalance of Elemental fighter.

--- Homework_7/src/fightersFactory/elementalStorm/ElementalOfEmptiness.java
+++ Homework_7/src/fightersFactory/elementalStorm/ElementalOfEmptiness.java
@@ -23,7 +23,7 @@ public class ElementalOfEmptiness implements ElementalFactory {
 
     @Override
     public Elemental createRandomElemental() {
-        return new Elemental(randomName.setRandomName(), randomInt.generate(1000,2000), randomInt.generate(25,30),
+        return new Elemental(randomName.setRandomName(), randomInt.generate(200,500), randomInt.generate(15,20),
                 randomDouble.generate(0.1,1), new ElementPower.Elements[]{elements});
     }
 }
